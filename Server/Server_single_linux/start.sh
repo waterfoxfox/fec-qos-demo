286ac61a2558ee92bf45162b73bd4266f3bb47b7
waterfoxfox <mediapro@126.com>
2019-10-23 16:23:17 +0800

    启动脚本前先执行stop，避免多次启动

--- Server/Server_single_linux/start.sh
+++ Server/Server_single_linux/start.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-cd /usr/local/MediaServer
-
+cd /usr/local/MediaServer;
+./nyx quit;
 echo 524288 > /proc/sys/net/core/rmem_max;
 echo 524288 > /proc/sys/net/core/wmem_max;
 ./nyx -c server.yaml; 
