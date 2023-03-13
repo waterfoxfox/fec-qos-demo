cdfd5a720bc2bd001738a91e61637d4a125c231e
gol_ele <39299075+waterfoxfox@users.noreply.github.com>
2023-03-13 11:15:37 +0800

    升级新版

--- Server/Server_single_linux/start.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-cd /usr/local/MediaServer;
-./nyx quit;
-echo 524288 > /proc/sys/net/core/rmem_max;
-echo 524288 > /proc/sys/net/core/wmem_max;
-./nyx -c server.yaml; 
--- Server/Server_single_linux/stop.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-cd /usr/local/MediaServer;
-./nyx quit;
